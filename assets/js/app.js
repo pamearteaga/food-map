eaf4ef2b48a34988f0a689de76411208ff3f11aa
pamearteaga <pe_arteaga@hotmail.com>
2017-12-14 10:48:17 -0300

    Completo :-)

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-//loader
+/*loader*/
 var time;
 
 function splash() {
@@ -11,29 +11,37 @@ function showPage() {
 }
 
 
-
+/*ready*/
 $(document).ready(function(){
-
+/*filtro categorias*/
   $("#categorias").change(function() {
-   var foodOption = $(this).val();
+   var foodOption = x
     $("#all").hide();
+  if(foodOption == "china"){
+     $('#chinafood').removeClass('hidden');
+     $('#venefood, #arabefood, #perufood').addClass('hidden');
+    }else if(foodOption == "venezolana"){
+      $('#venefood').removeClass('hidden');
+      $('#chinafood, #arabefood, #perufood ').addClass('hidden');
+    }else if(foodOption == "arabe"){
+      $('#arabefood').removeClass('hidden');
+      $('#venefood, #chinafood, #perufood').addClass('hidden');
+    }else if(foodOption == "peruana"){
+      $('#perufood').removeClass('hidden');
+      $('#venefood, #arabefood, #chinafood').addClass('hidden');
+    }else if(foodOption == "0"){
+      $('#all').show();
+      $('#venefood, #arabefood, #chinafood, #perufood').addClass('hidden');
+   };
+});
 
-      if(foodOption == "china"){
-         $('#chinafood').removeClass('hidden');
-         $('#venefood, #arabefood, #perufood').addClass('hidden');
-        }else if(foodOption == "venezolana"){
-            $('#venefood').removeClass('hidden');
-            $('#chinafood, #arabefood, #perufood ').addClass('hidden');
-        }else if(foodOption == "arabe"){
-            $('#arabefood').removeClass('hidden');
-            $('#venefood, #chinafood, #perufood').addClass('hidden');
-        }else if(foodOption == "peruana"){
-            $('#perufood').removeClass('hidden');
-            $('#venefood, #arabefood, #chinafood').addClass('hidden');
-        }else if(foodOption == "0"){
-            $('#all').show();
-            $('#venefood, #arabefood, #chinafood, #perufood').addClass('hidden');
-       };
-    });
+/*mouseover imagenes*/
+  $('.img-circle').mouseover(function () {
+    $(this).attr('style','opacity:0.5; background-color:#d9534f;');
+    }).mouseout(function () {
+    $(this).removeAttr('style');
+});
+
+
 });
 
